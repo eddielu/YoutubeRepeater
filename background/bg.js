6a4575b6687f3f8a2569351fda3333e10aa78d58
U-Eddie\eddielu <eddielu@Eddie.(none)>
2015-03-30 23:08:49 -0400

    Version 0.1

--- background/bg.js
+++ background/bg.js
@@ -2,8 +2,9 @@ console.log("Event page called");
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log("Got request");
+        //debugger;
         if (request.isHTML5) {
-            chrome.pageAction.show();
+            chrome.pageAction.show(sender.tab.id);
         }
     }
 );
