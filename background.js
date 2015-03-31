cd61429a3033a34577b4ed74504b2798218b9f00
U-Eddie\eddielu <eddielu@Eddie.(none)>
2015-03-31 10:12:45 -0400

    barebones pageaction popup

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log("Event page called");
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log("Got request");
-        if (request.isHTML5) {
-            chrome.pageAction.show();
-        }
-    }
-);
--- repeater.js
+++
@@ -1,4 +0,0 @@
-debugger;
-chrome.runtime.sendMessage({"isHTML5": true}, function (response) {});
-document.getElementsByTagName("video")[0].loop = true
-console.log("About to repeat video");
